8b85a896ac156b8681c0f2b6150fb0ce5249d045
Erzhan <aldamatov@gmail.com>
2022-03-12 13:58:26 +1100

    Changed Code

--- app.js
+++ app.js
@@ -1,66 +1,63 @@
-const addTodoBtn = document.querySelector('#add-new-todo-btn');
-const newTodoInput = document.querySelector('#new-todo-input');
-const ulTodoList = document.querySelector('#todo-list');
+const addTodoBtn = document.querySelector('#add-new-todo-btn')
+const newTodoInput = document.querySelector('#new-todo-input')
+const ulTodoList = document.querySelector('#todo-list')
 
-
-// const addTodoFn = function addTodoFn () {
-//     console.log(newTodoInput.value);
+// function addTodoFn () {
+//   console.log(newTodoInput.value)
 // }
 
+// const addTodoFn = function () {
+//   console.log(newTodoInput.value)
+// }
 
-addTodoBtn.addEventListener('click', function(){
-    const inputValue = newTodoInput.value;
+// addTodoBtn.onclick = function () {
+//   console.log(newTodoInput.value)
+// }
 
+addTodoBtn.addEventListener('click', function () {
+  // console.log(newTodoInput.value)
+  const inputValue = newTodoInput.value;
 
+  const listItem = document.createElement('li');
+  listItem.setAttribute('class', 'single-todo');
+  // listItem.className = 'single-todo';
+  // listItem.classList.add('single-todo');
 
-    const listItem = document.createElement('li');
-    listItem.setAttribute('class', 'single-todo');
+  // input checkbox
+  const checkBox = document.createElement('input');
+  checkBox.type = 'checkbox';
+  checkBox.setAttribute('class', 'single-todo-check');
+  checkBox.addEventListener('click', function () {
+    const classExist = checkBox.nextElementSibling.className.includes('cross-line')
+    if (classExist) {
+      checkBox.nextElementSibling.classList.remove('cross-line')
+    } else {
+      checkBox.nextElementSibling.classList.add('cross-line')
+    }
+  })
+  
+  // paragraph
+  const todoText = document.createElement('p')
+  todoText.innerText = inputValue;
+  todoText.setAttribute('class', 'single-todo-text');
+  
+  // delete btn
+  const deleteBtn = document.createElement('button')
+  deleteBtn.setAttribute('class', 'delete');
+  deleteBtn.innerText = "Delete";
+  deleteBtn.addEventListener('click', function () {
+    console.log('clicking the delete btn');
+    const listItem = deleteBtn.parentNode;
+    const ul = listItem.parentNode;
 
-    //Input checkbox
-    const checkBox = document.createElement('input');
-    checkBox.type = 'checkbox';
-    checkBox.setAttribute('class', 'single-todo-check');
-    checkBox.addEventListener('click', function (){
-        console.log('clicking the checkbox input');
+    ul.removeChild(listItem);
+  })
 
+  listItem.appendChild(checkBox)
+  listItem.appendChild(todoText)
+  listItem.appendChild(deleteBtn)
 
-        const classExist = checkBox.nextElementSibling.classList.includes('cross-line');
-        if (classExist) {
-            checkBox.nextElementSibling.classList.remove('cross-line');
-        }
-        else {
-            checkBox.nextElementSibling.classList.add('cross-line');
-        }
-        
+  ulTodoList.appendChild(listItem)
 
-    })
-
-
-    //Paragraph
-    const todoText = document.createElement('p');
-    todoText.innerText = inputValue;
-    todoText.setAttribute('class', 'single-todo-text');
-
-    //Delete Btn
-    const deleteBtn = document.createElement('button');
-    deleteBtn.setAttribute('class', 'delete');
-    deleteBtn.innerText = "Delete";
-    deleteBtn.addEventListener('click', function (){
-        console.log('clicking the delete btn');
-
-        const listItem = deleteBtn.parentNode;
-        const ul = listItem.parentNode;
-
-        ul.removeChild(listItem);
-    })
-
-    //Adding to Li
-    listItem.appendChild(checkBox);
-    listItem.appendChild(todoText);
-    listItem.appendChild(deleteBtn);
-
-    ulTodoList.appendChild(listItem);
-
-    newTodoInput.value = ' ';
-
+  newTodoInput.value = '';
 })
